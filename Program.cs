07fb3bf75ad9d87c741f86412643e8ff4621c681
Venkata Teja <142000714+TECH-HACKER16@users.noreply.github.com>
2024-02-19 19:33:29 +0530

    Simple Addition

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿class Addition
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Enter first number ");
+        int a = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Enter Second number ");
+        int b = Convert.ToInt32(Console.ReadLine());
+        int sum = a + b;
+        Console.WriteLine("Sum of given two numbers is  " + sum);
+    }
+}
