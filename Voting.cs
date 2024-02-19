d52ef6ee8f4aede54c5975add034b591d0f9f3f3
Venkata Teja <142000714+TECH-HACKER16@users.noreply.github.com>
2024-02-19 20:02:36 +0530

    Using if-else statements

---
+++ Voting.cs
@@ -0,0 +1,17 @@
+﻿using System;
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your Age : ");
+            int age = Convert.ToInt32(Console.ReadLine());           
+            if (age > 18)
+            {
+                Console.WriteLine("Your r eligible to vote");
+            }
+            else
+            {
+                Console.WriteLine("You r not eligible to vote");
+            }
+        }
+    }
