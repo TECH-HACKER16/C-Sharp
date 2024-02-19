f988ab6a6c5114d902c5f50c18602897047f7a86
Venkata Teja <142000714+TECH-HACKER16@users.noreply.github.com>
2024-02-19 19:51:15 +0530

    Obtaining Square root using Math class (Sqrt & Round)

--- Program.cs
+++ Program.cs
@@ -1,12 +1,11 @@
-﻿class Addition
+﻿using System;
+class Squareroot
 {
     static void Main(string[] args)
     {
-        Console.Write("Enter first number ");
-        int a = Convert.ToInt32(Console.ReadLine());
-        Console.Write("Enter Second number ");
-        int b = Convert.ToInt32(Console.ReadLine());
-        int sum = a + b;
-        Console.WriteLine("Sum of given two numbers is  " + sum);
+        Console.Write("Enter a number ");
+        double a = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Squareroot of given number is  " + Math.Round(Math.Sqrt(a),2));
+        //Rounding off the answer to 2 decimals
     }
 }
